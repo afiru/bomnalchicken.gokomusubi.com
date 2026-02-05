a1b4babedde796aa5a827cab42fc780e879ccfbc
Naoki Soga <n.soga@sunplacom>
2026-02-05 18:10:09 +0900

    --

--- public/wp-content/themes/WebPTemplatebyMarcat/include/layouts/bomcafe/01_bomcafefv.php
+++ public/wp-content/themes/WebPTemplatebyMarcat/include/layouts/bomcafe/01_bomcafefv.php
@@ -1,7 +1,7 @@
 <div class="d_flex j_center ali_center catFv">
     <!-- bg:../img/catFv.png -->
     <section class="secCatFv">
-        <h1 class="cl_EE952D Mochiy t_center fw_800 h2SecCatFv"><?php echo esc_html('ボムカフェについて'); ?></h1>
+        <h1 class="cl_EE952D Mochiy t_center fw_400 h2SecCatFv"><?php echo esc_html('ボムカフェについて'); ?></h1>
         <div class="bg_fff brdSecCatFv"></div>
         <h3 class="cl_E9483E t_center fw_800 en h3SecCatFv"><?php echo esc_html('BomCafe'); ?></h3>
     </section>
