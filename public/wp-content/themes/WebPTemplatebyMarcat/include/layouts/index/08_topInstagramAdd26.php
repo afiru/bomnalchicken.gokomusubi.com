5636d6b2dc36f2a8d5f4aa8bb42f1453b0b1d0cf
Naoki Soga <n.soga@sunplacom>
2026-07-07 18:44:54 +0900

    ---

---
+++ public/wp-content/themes/WebPTemplatebyMarcat/include/layouts/index/08_topInstagramAdd26.php
@@ -0,0 +1,5 @@
+<?php
+/**
+ * Template part: 08_topInstagramAdd26
+ */
+?>
--- public/wp-content/themes/WebPTemplatebyMarcat/tempTopPage.php
+++ public/wp-content/themes/WebPTemplatebyMarcat/tempTopPage.php
@@ -8,18 +8,16 @@
 <?php get_template_part('include/common/header/header'); ?>
 <main class="mainIndex">
     <?php if (have_posts()) while (have_posts()) : the_post();  ?>
-        <script>
+    <script>
 
-        </script>
+    </script>
 
-        <?php get_template_part('include/layouts/index/01_topFv'); ?>
-        <?php get_template_part('include/layouts/index/02_topNews'); ?>
-        <?php get_template_part('include/layouts/index/03_topAbout'); ?>
-        <?php get_template_part('include/layouts/index/04_topFloor'); ?>
-        <?php get_template_part('include/layouts/index/05_topCalendar'); ?>
-        <?php get_template_part('include/layouts/index/07_topInstagram'); ?>
-
-
+    <?php get_template_part('include/layouts/index/01_topFv'); ?>
+    <?php get_template_part('include/layouts/index/02_topNews'); ?>
+    <?php get_template_part('include/layouts/index/03_topAbout'); ?>
+    <?php get_template_part('include/layouts/index/04_topFloor'); ?>
+    <?php get_template_part('include/layouts/index/05_topCalendar'); ?>
+    <?php get_template_part('include/layouts/index/07_topInstagram'); ?>
     <?php endwhile; ?>
     <?php get_template_part('include/layouts/index/100_faq'); ?>
     <?php get_template_part('include/layouts/index/99_booking'); ?>
