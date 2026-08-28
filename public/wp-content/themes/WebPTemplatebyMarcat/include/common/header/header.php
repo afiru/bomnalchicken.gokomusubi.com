65463a2453974105c16633ba1fafcd34dc332430
kinoshita-k-sunpla <kinoshita-k@sunpla.com>
2026-08-28 18:59:33 +0900

    ぼむちゃん

--- public/wp-content/themes/WebPTemplatebyMarcat/include/common/header/header.php
+++ public/wp-content/themes/WebPTemplatebyMarcat/include/common/header/header.php
@@ -3,23 +3,23 @@
 
 <head>
     <script type="application/ld+json">
-    {
-        "@context": "https://schema.org",
-        "@type": "Restaurant",
-        "name": "ボムナルチキン",
-        "url": "https://bomnalchicken.machimusubi.com",
-        "image": "https://bomnalchicken.machimusubi.com/wp-content/uploads/2025/11/Frame-5-2.jpg",
-        "description": "兵庫・明石市のテイクアウト専門店「ボムナルチキン」公式サイト。ヤンニョムチキン・ハニーバターチキン・サクサクフライドなど韓国人気メニューを持ち帰りOK。ランチや夕食、お土産にもぴったり。Instagramでも最新メニュー＆キャンペーン発信中！",
-        "servesCuisine": "Korean Chicken",
-        "sameAs": [
-            "https://www.instagram.com/bomnalchicken/?hl=ja"
-        ],
-        "address": {
-            "@type": "PostalAddress",
-            "addressLocality": "明石市",
-            "addressCountry": "JP"
+        {
+            "@context": "https://schema.org",
+            "@type": "Restaurant",
+            "name": "ボムナルチキン",
+            "url": "https://bomnalchicken.machimusubi.com",
+            "image": "https://bomnalchicken.machimusubi.com/wp-content/uploads/2025/11/Frame-5-2.jpg",
+            "description": "兵庫・明石市のテイクアウト専門店「ボムナルチキン」公式サイト。ヤンニョムチキン・ハニーバターチキン・サクサクフライドなど韓国人気メニューを持ち帰りOK。ランチや夕食、お土産にもぴったり。Instagramでも最新メニュー＆キャンペーン発信中！",
+            "servesCuisine": "Korean Chicken",
+            "sameAs": [
+                "https://www.instagram.com/bomnalchicken/?hl=ja"
+            ],
+            "address": {
+                "@type": "PostalAddress",
+                "addressLocality": "明石市",
+                "addressCountry": "JP"
+            }
         }
-    }
     </script>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta content="text/css" http-equiv="Content-Style-Type" />
@@ -29,7 +29,7 @@
     <meta http-equiv="Content-Language" content="<?php bloginfo('language'); ?>">
     <?php $user = get_user_by('id', 1); ?>
     <?php if (!empty($user->first_name)): ?>
-    <meta name="Author" content="<?php echo $user->first_name . $user->last_name; ?>">
+        <meta name="Author" content="<?php echo $user->first_name . $user->last_name; ?>">
     <?php endif; ?>
     <meta name="format-detection" content="telephone=no">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -51,7 +51,7 @@
     <title><?php echo get_the_site_title(get_php_customzer('seo_title')); ?></title>
 
     <?php if (is_single()): ?>
-    <?php
+        <?php
         $the_content = get_post(get_the_ID())->post_content;
         $the_content = strip_tags($the_content);
         $the_content = stripslashes($the_content);
@@ -66,52 +66,52 @@
             $ogthumbs = $ogthumbs;
         }
         ?>
-    <script type="application/ld+json">
-    {
-        "@context": "https://schema.org",
-        "@type": "BlogPosting",
-        "headline": "<?php echo get_the_title(get_the_ID()); ?>",
-        "description": "<?php echo $the_content; ?>",
-        "author": {
-            "@type": "Person",
-            "name": "<?php echo esc_html(get_bloginfo('name')); ?>"
-        },
-        "publisher": {
-            "@type": "Organization",
-            "name": "<?php echo esc_html(get_bloginfo('name')); ?>",
-            "logo": {
-                "@type": "ImageObject",
-                "url": "<?php echo $ogthumbs; ?>"
+        <script type="application/ld+json">
+            {
+                "@context": "https://schema.org",
+                "@type": "BlogPosting",
+                "headline": "<?php echo get_the_title(get_the_ID()); ?>",
+                "description": "<?php echo $the_content; ?>",
+                "author": {
+                    "@type": "Person",
+                    "name": "<?php echo esc_html(get_bloginfo('name')); ?>"
+                },
+                "publisher": {
+                    "@type": "Organization",
+                    "name": "<?php echo esc_html(get_bloginfo('name')); ?>",
+                    "logo": {
+                        "@type": "ImageObject",
+                        "url": "<?php echo $ogthumbs; ?>"
+                    }
+                },
+                "mainEntityOfPage": {
+                    "@type": "WebPage",
+                    "@id": "<?php echo home_url('/'); ?>"
+                },
+                "datePublished": "<?php echo get_the_date('y-m-d'); ?>",
+                "dateModified": "<?php echo get_the_date('y-m-d'); ?>"
             }
-        },
-        "mainEntityOfPage": {
-            "@type": "WebPage",
-            "@id": "<?php echo home_url('/'); ?>"
-        },
-        "datePublished": "<?php echo get_the_date('y-m-d'); ?>",
-        "dateModified": "<?php echo get_the_date('y-m-d'); ?>"
-    }
-    </script>
+        </script>
     <?php endif; ?>
 
 
 
     <script>
-    var home_url = "<?php echo home_url('/'); ?>";
-    var theme_url = "<?php echo esc_url(get_bloginfo('template_url')); ?>";
-    var rest_url = "<?php echo home_url('/wp-json/wp/v2/'); ?>";
-    var calendar_y = "<?php echo date('Y'); ?>";
-    var calendar_m = "<?php echo date('m'); ?>";
-    /*
-        <?php foreach (scf::get('eventdates', 226) as $fields): ?>
-            <?php $result[] = '"' . date("md", strtotime($fields['eventdate'])) . '"'; ?>
-        <?php endforeach; ?>
-        */
-    <?php if (!empty($result[0])): ?>
-    var holiday = [<?php echo implode(',', $result); ?>];
-    <?php else: ?>
-    var holiday = [""];
-    <?php endif; ?>
+        var home_url = "<?php echo home_url('/'); ?>";
+        var theme_url = "<?php echo esc_url(get_bloginfo('template_url')); ?>";
+        var rest_url = "<?php echo home_url('/wp-json/wp/v2/'); ?>";
+        var calendar_y = "<?php echo date('Y'); ?>";
+        var calendar_m = "<?php echo date('m'); ?>";
+        /*
+            <?php foreach (scf::get('eventdates', 226) as $fields): ?>
+                <?php $result[] = '"' . date("md", strtotime($fields['eventdate'])) . '"'; ?>
+            <?php endforeach; ?>
+            */
+        <?php if (!empty($result[0])): ?>
+            var holiday = [<?php echo implode(',', $result); ?>];
+        <?php else: ?>
+            var holiday = [""];
+        <?php endif; ?>
     </script>
 </head>
 
@@ -122,6 +122,11 @@
 
 
         <div class="wapper pageWap">
+            <div class="pc_only pofixed picIndexMainLxn">
+                <figure class="d_flex j_center ali_center picIndexMainFx">
+                    <img loading="lazy" src="<?php echo esc_url(get_bloginfo('template_url')); ?>/img/picIndexMainFx.png" alt="<?php bloginfo('name'); ?>" width="803" height="803">
+                </figure>
+            </div>
             <div class="cntPageLxn bgCntPageLxn">
                 <!--
       bg:../bgCntPageLxn.png
